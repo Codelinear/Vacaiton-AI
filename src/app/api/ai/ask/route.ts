eda8d90841cc1164f3a2170833df666c057d6425
uzefm127 <uzef@codelinear.com>
2024-05-31 22:16:55 +0530

    updated the api

--- src/app/api/ai/ask/route.ts
+++ src/app/api/ai/ask/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { StreamingTextResponse, streamText } from "ai";
-import { createAnthropic } from "@ai-sdk/anthropic";
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
 
 export const maxDuration = 60;
 
@@ -8,8 +8,8 @@ export const POST = async (req: NextRequest) => {
   try {
     const { prompt, endDate, reason, startDate } = await req.json();
 
-    const anthropic = createAnthropic({
-      apiKey: process.env.ANTHROPIC_API_KEY,
+    const google = createGoogleGenerativeAI({
+      apiKey: process.env.GEMINI_API_KEY,
     });
 
     const systemPrompt = `Plan a ${reason} vacation itinerary for me from ${
@@ -19,13 +19,12 @@ export const POST = async (req: NextRequest) => {
     } in ${prompt}. Include popular places to visit and activities to do. You must give the response in a list format so that the plan is visually understandable. If someone gives an invalid destination which is not there then you'll simply say "I'm not getting the valid destination to design a vacation."`;
 
     const response = await streamText({
-      model: anthropic("claude-3-sonnet-20240229"),
+      model: google("models/gemini-1.5-flash-latest"),
       temperature: 0,
       prompt: systemPrompt,
     });
 
     return new StreamingTextResponse(response.toAIStream());
-    // return response.toAIStreamResponse();
   } catch (error: any) {
     console.log(error);
     return NextResponse.json(
